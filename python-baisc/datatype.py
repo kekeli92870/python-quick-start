7545ee9405d32f87fe2a2e247afcab4e5199134a
柯柯利 <96953101+kekeli92870@users.noreply.github.com>
2022-01-13 00:54:08 +0800

    Python變數與資料型態

---
+++ python-baisc/datatype.py
@@ -0,0 +1,38 @@
+# 資料: 程式的基本單位
+# 數字
+3456
+3.5
+# 字串
+"測試中文"
+"Hello World"
+# 布林值
+True
+False
+# 有順序,可變動的列表 List
+[3,4,5]
+["Hello","World"]
+# 有順序,不可變動的列表 Tuple
+(3,4,5)
+("Hello","World")
+# 集合 Set
+{3,4,5}
+{"Hello","World"}
+# 字典 Dictionary
+{
+    "data":"資料",
+    "apple":"蘋果"
+}
+
+# 變數: 用來儲存資料的自訂名稱
+# 變數名稱=資料
+x=3
+# print(資料)
+print(3)
+print(x)
+print([3,4,5])
+x=True  # 取代舊的資料
+print(x)
+x="hello world"
+print(x)
+x={3,4,5}
+print(x)
