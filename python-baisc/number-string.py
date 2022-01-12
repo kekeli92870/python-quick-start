d59b9af30334ab1d14614f7e2b30096b94c4afc4
柯柯利 <96953101+kekeli92870@users.noreply.github.com>
2022-01-13 00:54:43 +0800

    Python數字和字串

---
+++ python-baisc/number-string.py
@@ -0,0 +1,53 @@
+# 數字運算
+x=3+6
+print(x)
+x=3-6
+print(x)
+x=3*6
+print(x)
+x=3/6 # 小數除法
+print(x)
+x=7//6 # 整數除法
+print(x)
+x=2**3 #2的3次方
+print(x)
+x=2**0.5 #2的開根號
+print(x)
+x=7%3 # 取餘數
+print(x)
+y=2+3
+print(y)
+# y=y+1
+y+=1 #將變數的數字+1
+#y-=1 #y*=1 #y/=1
+print(y)
+
+# 字串運算
+s="Hello"
+print(s)
+s='Hello'
+print(s)
+s="Hel\"lo" # \ 使用跳脫符號
+print(s)
+s="Hello"+"World"
+print(s)
+s="Hello" "World" # 用空格代表字串連接
+print(s)
+s="Hello\nWorld" #\n 代表換行
+print(s)
+s="""hello
+123
+456
+
+
+world""" # 用3個雙引號或單引號代表換行
+print(s)
+s="Hello"*3 # 字串重覆3次
+print(s)
+# 字串會對內部的字元編號(索引),從 0 開始算起
+s="Hello"
+print(s[0]) # 印出 H
+print(s[1:4]) # 印出 ell
+print(s[1:]) # 印出 ello
+print(s[:4]) # 印出 Hell
+
